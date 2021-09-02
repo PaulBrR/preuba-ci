db500b2f599a63f9de4916fcf0705496f9bdf518
PaulBrR <paalbrisenoro@ittepic.edu.mx>
2021-09-02 01:09:47 -0600

    Agregando nueva funcion

--- __test__/challenge1.test.js
+++ __test__/challenge1.test.js
@@ -7,4 +7,13 @@ test('Debería regresar 8 al pasar como argumentos 6 y 2', () => {
     let result = suma(a,b);
 
     expect(result).toBe(8);
+});
+
+test('dEBERÍA REGRESAR 4 A al pasar como argumentos 8 y 4', () => {
+    let a= 8;
+    let b = 4;
+
+    let result = resta(a,b);
+
+    expect(result).toBe(4);
 })
